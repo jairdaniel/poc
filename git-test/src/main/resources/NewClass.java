95f3619923aecccab132329224a5bbf796bdd716
shiotodan84 <jair_daniel@yahoo.com>
2016-02-25 10:54:49 -0600

    github, adding sysout on main

--- git-test/src/main/resources/NewClass.java
+++ git-test/src/main/resources/NewClass.java
@@ -14,6 +14,7 @@ public class NewClass {
 	}
 	
 	public static void main(String[] args) {
+		System.out.println("A change without conflict on the same line. Done from Github");
 		System.out.println("Anew class");
 		System.out.println("A new change from Github");
 	}
