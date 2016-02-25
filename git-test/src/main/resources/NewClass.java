100ea07166576466b179db9e734b853c24910ba4
JairDaniel Correa Sanchez <JairDaniel.X.CorreaSanchez@healthpartners.com>
2016-02-25 10:41:21 -0600

    Merging changes

--- git-test/src/main/resources/NewClass.java
+++ git-test/src/main/resources/NewClass.java
@@ -1,6 +1,10 @@
 
 public class NewClass {
 
+	public void method(){
+		System.out.println("A useless method");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Anew class");
 		System.out.println("A new change from Github");
