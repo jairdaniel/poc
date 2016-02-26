340bf30fdab28a496de948b46b6d10d6a5951807
jairdaniel <jair.daniel@gmail.com>
2016-02-25 19:38:00 -0600

    Conflict solution from Eclipse Macbook

--- git-test/src/main/resources/Clase.java
+++ git-test/src/main/resources/Clase.java
@@ -1,8 +1,13 @@
 
 public class Clase {
 
+	private void anotherMethod(){
+		System.out.println("anotherMethod");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello Clase");
 		System.out.println("Another sysout");
+		System.out.println("Another print");
 	}
 }
